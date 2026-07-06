3e3c1c13c0d2a26794fa2a8f45da5520397f1b33
LorenGrz <lorenzograizzaro55@gmail.com>
2026-07-06 00:46:26 -0300

    Agrego ejercicios de desestructuracion

--- ejercicios/exercises/basic/05-desestructuracion.js
+++ ejercicios/exercises/basic/05-desestructuracion.js
@@ -1,20 +1,37 @@
 // 1. Usa desestructuración para extraer los dos primeros elementos de un array
-
+let array = [1, 2, 3, 4, 5, 6]
+let array2 = [7, 8, 9, 10]
+let [el1, , el3] = array
+console.log(el1)
+console.log(el3)
 // 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
-
+let [el2, , , , , , , el7 = 0] = array
+console.log(el7)
 // 3. Usa desestructuración para extraer dos propiedades de un objeto
-
-// 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
-//    a nuevas variables con nombres diferentes
-
-// 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
-
+let person = {
+  name: "Loren",
+  age: 23,
+  alias: "LorenGrz",
+}
+let { name: nombre, alias: apodo } = person
+console.log(nombre)
+console.log(apodo)
 // 6. Usa propagación para combinar dos arrays en uno nuevo
-
+let array4 = [...array, ...array2]
+console.log(array4)
 // 7. Usa propagación para crear una copia de un array
-
+let array3 = [...array]
 // 8. Usa propagación para combinar dos objetos en uno nuevo
-
+let persona2 = {
+  name1: "Loren",
+  age1: 23,
+  alias1: "LorenGrz",
+}
+let personaCombinada = { ...person, ...persona2 }
+console.log(personaCombinada)
 // 9. Usa propagación para crear una copia de un objeto
-
+let copiaPerson = { ...person }
 // 10. Combina desestructuración y propagación
+let { age, name, alias: apodo2, alias1: apodo3 } = { ...person, ...persona2 }
+console.log(apodo2)
+console.log(apodo3)
--- ejercicios/js/11-desestructuracion.js
+++ ejercicios/js/11-desestructuracion.js
@@ -13,9 +13,37 @@ myValue = myArray[1]
 // Destructuracion
 // Sintaxis arrays y valores predeterminados e ignoerar elementos
 let [myValue0 = 0, , myValue2] = myArray
-console.log(myValue0)
-console.log(myValue2)
+// console.log(myValue0)
+// console.log(myValue2)
 
 // sintaxis objects
 
-let { nombre, edad, apodo } = person
+let { name, age, alias } = person
+console.log(name)
+console.log(age)
+console.log(alias)
+
+// sintaxis objects con nombres nuevos
+let { name: nombre, age: edad, alias: apodo } = person
+console.log(nombre)
+console.log(edad)
+console.log(apodo)
+
+// Propagacion (...)
+
+let myArray2 = [...myArray] // Copia
+
+let myArray3 = [...myArray, 6, 7]
+console.log(myArray2)
+console.log(myArray3)
+
+// Combinacion de arrays
+let myArray4 = [...myArray, ...myArray2, ...myArray3]
+console.log(myArray4)
+
+// Sintaxis objects
+let person4 = { ...person, email: "lorenzograizzaro55@gmail.com" }
+console.log(person4)
+
+// Copia de objects
+let person5 = { ...person }
