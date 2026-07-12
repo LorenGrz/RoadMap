a1c18aae8b3028cb8a39d94769dbfe185af61149
LorenGrz <lorenzograizzaro55@gmail.com>
2026-07-12 19:50:40 -0300

    Sigo practicando ahora objtos y classes avanzadas

--- js/hello-javascript/Intermediate/03-advanced-structures-exercises.js
+++ js/hello-javascript/Intermediate/03-advanced-structures-exercises.js
@@ -24,17 +24,66 @@ const numerosModificados = numeros
 console.log(numerosModificados)
 
 // 3. Utiliza flat y flatMap para crear un ejemplo diferente al de la lección
+let arrayComplicado = [1, [2, [3, [4, [5]]]]]
+console.log(arrayComplicado.flat(3))
+
+let arrayDePalabras = ["Que pasa chabales", "estos son 2 strings"]
+console.log(arrayDePalabras.flatMap((str) => str.split(" ")))
 
 // 4. Ordena un array de números de mayor a menor
+console.log(numeros.sort((a, b) => b - a))
 
 // 5. Dados dos sets, encuentra la unión, intersección y diferencia de ellos
-
+const mySet = new Set([2, 3, 4, 5, 6, 4, 6, 7, 8])
+const mySet2 = new Set([7, 8, 9, 10, 23])
+const unionDeSets = new Set([...mySet, ...mySet2])
+console.log(unionDeSets)
+const interseccionDeSets = new Set([...mySet].filter((e) => mySet2.has(e)))
+console.log(interseccionDeSets)
+const diferenciaDeSets = new Set([...mySet].filter((e) => !mySet2.has(e)))
+console.log(interseccionDeSets)
+const setsSinLasIntersecciones = new Set([
+  ...[...mySet].filter((e) => !mySet2.has(e)),
+  ...[...mySet2].filter((e) => !mySet.has(e)),
+])
+console.log(setsSinLasIntersecciones)
 // 6. Itera los resultados del ejercicio anterior
+Array.from(unionDeSets).forEach((n) => console.log(`El numero unido es: ${n}`))
+Array.from(interseccionDeSets).forEach((n) =>
+  console.log(`El numero intersectado es: ${n}`),
+)
+Array.from(diferenciaDeSets).forEach((n) =>
+  console.log(`El numero que no esta en el primer set es: ${n}`),
+)
+Array.from(setsSinLasIntersecciones).forEach((n) =>
+  console.log(`El numero que no esta en los 2 a la vez es: ${n}`),
+)
 
 // 7. Crea un mapa que almacene información se usuarios (nombre, edad y email) e itera los datos
-
+const data = new Map([
+  ["Lucero", [28, "lucerorocha@gmail.com"]],
+  ["Lorenzo", [23, "lorenzograizzaro55@gmail.com"]],
+  ["Ania", [10, "anyaforger@gmail.com"]],
+])
+data.forEach((value, key) => console.log(`${key}: ${value}`))
 // 8. Dado el mapa anterior, crea un array con los nombres
-
+const arrayDeNombre = new Array(...data.keys())
+console.log(arrayDeNombre)
 // 9. Dado el mapa anterior, obtén un array con los email de los usuarios mayores de edad y transfórmalo a un set
+const emailsMayores = new Set(
+  [...data.values()]
+    .filter(([edad, email]) => edad >= 18)
+    .map(([edad, email]) => email),
+)
+console.log(emailsMayores)
 
 // 10. Transforma el mapa en un objeto, a continuación, transforma el objeto en un mapa con clave el email de cada usuario y como valor todos los datos del usuario
+let objeto = Object.fromEntries(data)
+console.log(objeto)
+let mapa = new Map(
+  Object.entries(objeto).map(([nombre, [edad, mail]]) => [
+    mail,
+    [nombre, edad],
+  ]),
+)
+console.log(mapa)
--- js/hello-javascript/Intermediate/06-advanced-objects-classes-exercises.js
+++ js/hello-javascript/Intermediate/06-advanced-objects-classes-exercises.js
@@ -4,21 +4,131 @@ Vídeo: https://youtu.be/iJvLAZ8MJ2E?t=11832
 */
 
 // 1. Agregega una función al prototipo de un objeto
+const objetoEjercicios = {
+  nombre: "Lorenzo",
+  edad: 23,
+  email: "lorenzograizzaro55@gmail.com",
+}
 
+objetoEjercicios.caminar = () => console.log("Estoy caminando")
+objetoEjercicios.caminar()
 // 2. Crea un objeto que herede de otro
+const otroObjeto = { apellido: "Graizzaro" }
+const objetoHeredado = Object.assign(otroObjeto, objetoEjercicios)
+console.log(`mi apellido es ${objetoHeredado.apellido}`)
+objetoHeredado.caminar()
 
 // 3. Define un método de instancia en un objeto
+function MetodoDeInstancia(nombre, edad) {
+  this.nombre = nombre
+  this.edad = edad
+}
+MetodoDeInstancia.prototype.saludar = () => console.log("Hola")
+let objetoInstanciado = new MetodoDeInstancia("lorenzo", 23)
+objetoInstanciado.saludar()
 
 // 4. Haz uso de get y set en un objeto
+class ObjetoConGetYSet {
+  #edad
+  #nombre
+  constructor(edad = 23, nombre = "Lorenzo") {
+    this.#edad = edad
+    this.#nombre = nombre
+  }
+  get nombre() {
+    return this.#nombre
+  }
+  set edad(edad) {
+    this.#edad = edad
+  }
+  get edad() {
+    return this.#edad
+  }
+}
+const objetoInstanciado2 = new ObjetoConGetYSet()
+console.log(objetoInstanciado2.nombre)
+console.log(objetoInstanciado2.edad)
+objetoInstanciado2.edad = 25
+console.log(objetoInstanciado2.edad)
 
 // 5. Utiliza la operación assign en un objeto
-
+const nuevoObjeto = {
+  perro: "Chimuela",
+}
+const objetoCombinado = Object.assign(nuevoObjeto, objetoInstanciado2)
+objetoCombinado.nombre = "Nuevo nombre"
+console.log(objetoCombinado.nombre)
+ObjetoConGetYSet.prototype.saltar = () => console.log("Estoy saltando")
+ObjetoConGetYSet.prototype.nacionalidad = "Argentina"
+console.log(ObjetoConGetYSet)
+const objetoInstanciado3 = new ObjetoConGetYSet()
+objetoInstanciado3.saltar()
+console.log(objetoInstanciado3.nacionalidad)
 // 6. Crea una clase abstracta
+class ClaseAbstractajs {
+  constructor(nombre, edad) {
+    if (new.target === ClaseAbstractajs) {
+      throw new Error("No se puede instanciar una clase abstracta")
+    }
+    this.nombre = nombre
+    this.edad = edad
+  }
+  metodoAbstracto() {
+    throw new Error(
+      "Se debe de sobreescribir el metodo de la clase abstracta en las clases heredadas",
+    )
+  }
+}
 
 // 7. Utiliza polimorfismo en dos clases diferentes
+class ClaseHeredada1 extends ClaseAbstractajs {
+  metodoAbstracto() {
+    console.log("Metodo Sobreescrito")
+  }
+}
+class ClaseHeredada2 extends ClaseAbstractajs {
+  metodoAbstracto() {}
+}
 
 // 8. Implementa un Mixin
+const MixinNuevo = {
+  serRePiola() {
+    console.log("SoyRePiola")
+  },
+}
 
+Object.assign(ClaseHeredada1.prototype, MixinNuevo)
+const objetoInstanciadoConMixin = new ClaseHeredada1()
+objetoInstanciadoConMixin.serRePiola()
 // 9. Crea un Singleton
+class Singleton {
+  constructor(nombre) {
+    if (Singleton.instance) {
+      return Singleton.instance
+    }
+    this.nombre = nombre
+    Singleton.instance = this
+  }
+}
+// de esta manera solo se puede instanciar una vez el nombre, mantiene la Sesion, en caso de modificarlo hay que utilizar un metodo que lo modifique, cosa de instanciarlo luego de otra manera
 
 // 10. Desarrolla un Proxy
+const proxy = {
+  get(target, property) {
+    console.log(`Estoy por mostrar la propiedad ${property}`)
+    return target[property]
+  },
+  set(target, property, value) {
+    if (property === "edad" && value < 18) {
+      throw new Error("Debe ser mayor de edad para ingresar la edad")
+    }
+    console.log(`Estoy seteando la propiedad ${property}`)
+    target[property] = value
+  },
+}
+
+const objetoProtegidoPorProxy = new Proxy(
+  new ClaseHeredada1("Lorenzo", 23),
+  proxy,
+)
+console.log(objetoProtegidoPorProxy.nombre)
