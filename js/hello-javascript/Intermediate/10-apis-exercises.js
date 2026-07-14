bf8c294548e3450bcef900803077f84f48e1334b
LorenGrz <lorenzograizzaro55@gmail.com>
2026-07-13 23:10:52 -0300

    Agrego ejercicios de API

--- js/hello-javascript/Intermediate/10-apis-exercises.js
+++ js/hello-javascript/Intermediate/10-apis-exercises.js
@@ -4,12 +4,59 @@ Vídeo: https://youtu.be/iJvLAZ8MJ2E?t=18710
 */
 
 // 1. Realiza una petición GET con fetch() a JSONPlaceholder y muestra en la consola la lista de publicaciones
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json()
+//     } else {
+//       throw new Error(
+//         `El estado de la peticion es incorrecto: ${response.status}`,
+//       )
+//     }
+//   })
+//   .then((data) => {
+//     console.log(data)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
 // 2. Modifica el ejercicio anterior para que verifique si la respuesta es correcta usando response.ok. Si no lo es, lanza y muestra un error
 
 // 3. Reescribe el ejercicio 1 usando la sintaxis async/await en lugar de promesas
+// async function getPublicaciones() {
+//   try {
+//     let response = await fetch("https://jsonplaceholder.typicode.com/posts")
+//     if (!response.ok) {
+//       throw new Error(
+//         `El estado de la peticion es incorrecto: ${response.status}`,
+//       )
+//     }
+//     let data = await response.json()
+//     console.log(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+// getPublicaciones()
 
 // 4. Realiza una petición POST a JSONPlaceholder para crear una nueva publicación. Envía un objeto con propiedades como title o body
+const publicacion = {
+  title: "Este es el titulo",
+  body: "Este es el body",
+}
+async function crearPublicacion(publicacion) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(publicacion),
+  })
+  const data = await response.json()
+  console.log(data)
+}
+crearPublicacion(publicacion)
 
 // 5. Utiliza el método PUT para actualizar completamente un recurso (por ejemplo, modificar una publicación) en JSONPlaceholder
 
